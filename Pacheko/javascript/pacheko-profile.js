63f34d0ed34f10f3f088c93aab588937b4ffc6a2
pacheko8 <56631465+pacheko8@users.noreply.github.com>
2019-11-12 08:51:37 +0300

    Add files via upload

---
+++ Pacheko/javascript/pacheko-profile.js
@@ -0,0 +1,27 @@
+/*var modal = document.getElementById('modal');
+
+var btn = document.getElementById("about");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+ 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}*/
+function dialog(){
+
+	alert("Thank you ,Pacheko Inc. has just received your message");
+}
